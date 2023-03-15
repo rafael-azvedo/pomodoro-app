46a4f9f3db39046cd42525a6d11bb5ebda8cd7aa
Rafael de Azevedo <rafael.deazevedo01@gmail.com>
2023-03-15 20:13:21 -0300

    Adicionando a funcao que renderiza o momento da tecnica via js

--- Pomodoro-app/script.js
+++ Pomodoro-app/script.js
@@ -4,9 +4,13 @@ var sec = 00;
 function renderClock(m , s){
     document.getElementById("clock").innerHTML = m + "m " + s + "s";
 }
+function renderTitle(title){
+    document.getElementById("title").innerHTML = title;
+}
 
 function start(){
     min--;
     renderClock(min, sec);
+    renderTitle("pomodoro")
 }
 console.log("teste btn")
