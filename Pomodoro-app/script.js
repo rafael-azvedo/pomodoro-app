323c0e65685a7825c0713ad3771302455dcdf29e
Rafael de Azevedo <rafael.deazevedo01@gmail.com>
2023-03-15 20:26:56 -0300

    Funcionalidade basica do temporizador implementada

--- Pomodoro-app/script.js
+++ Pomodoro-app/script.js
@@ -1,5 +1,6 @@
-var min = 25;
+var min = 10;
 var sec = 00;
+renderClock(min, sec);
 
 function renderClock(m , s){
     document.getElementById("clock").innerHTML = m + "m " + s + "s";
@@ -7,10 +8,40 @@ function renderClock(m , s){
 function renderTitle(title){
     document.getElementById("title").innerHTML = title;
 }
-
 function start(){
     min--;
+    sec = 59;
     renderClock(min, sec);
-    renderTitle("pomodoro")
+    renderTitle("Pomodoro");
+    pomodoroInterval = setInterval(pomodoroClock, 10);
 }
-console.log("teste btn")
+function pomodoroClock(){
+    if(sec <= 0 && min <= 0){
+        renderTitle("Pausa");
+        clearInterval(pomodoroInterval);
+        min = 5;
+        restInterval = setInterval(restClock, 10);
+    }else if(sec <= 0){
+        min--;
+        sec = 59;
+        renderClock(min, sec);
+    }else{
+        sec--;
+        renderClock(min, sec);
+    }
+}
+function restClock(){
+    if(sec <= 0 && min <= 0){
+        renderTitle("Pomodoro");
+        clearInterval(restInterval);
+        min = 25
+        pomodoroInterval = setInterval(pomodoroClock, 10)
+    }else if(sec <= 0){
+        min--;
+        sec = 59;
+        renderClock(min, sec);
+    }else{
+        sec--;
+        renderClock(min, sec);
+    }
+}
