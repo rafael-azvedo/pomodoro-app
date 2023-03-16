482f72413fae75490925d28acd9f3a163f68e7a0
Rafael de Azevedo <rafael.deazevedo01@gmail.com>
2023-03-15 23:26:32 -0300

    Implementado a edicao de tempo para o pomodoro

--- Pomodoro-app/script.js
+++ Pomodoro-app/script.js
@@ -1,7 +1,13 @@
-var min = 1;
+var initialMin = 25;
 var sec = 00;
-renderClock(min, sec);
 var bell = document.getElementById("bell");
+var editDiv = document.getElementById("edit");
+var editBtn = document.getElementById("editBtn");
+var saveBtn = document.getElementById("startBtn");
+var resetBtn = document.getElementById("resetBtn");
+var edited = false;
+var min = initialMin;
+var clockTick = 200;
 
 window.onload = function() {
     Notification.requestPermission();    
@@ -12,13 +18,71 @@ function renderClock(m , s){
 function renderTitle(title){
     document.getElementById("title").innerHTML = title;
 }
+function edit(){
+    editDiv.style.display = "block";
+    editBtn.style.display = "none";
+    saveBtn.style.display = "none";
+}
+function save(){
+    let editMin = document.getElementById("min").value;
+    let editSec = document.getElementById("sec").value;
+    if(editMin == 0 && editSec == 0){
+        editDiv.style.display = "none";
+        editBtn.style.display = "inline";
+        saveBtn.style.display = "inline"; 
+    }else{
+        initialMin = editMin;
+        sec = editSec;
+    }
+    renderClock(initialMin, sec);
+    edited = true;
+    editDiv.style.display = "none";
+    editBtn.style.display = "inline";
+    saveBtn.style.display = "inline";
+}
+function reset(){
+    if(pomodoroInterval){
+        clearInterval(pomodoroInterval);
+    }else{
+        clearInterval(restInterval)
+    }
+    initialMin = 25;
+    sec = 00;
+    document.getElementById("min").value = 0;
+    document.getElementById("sec").value = 0;
+    renderClock(initialMin, sec);
+    editBtn.style.display = "inline";
+    saveBtn.style.display = "inline";
+    resetBtn.style.display = "none";
+}
 function start(){
-    min--;
-    sec = 59;
-    renderClock(min, sec);
-    renderTitle("Pomodoro");
-    pomodoroInterval = setInterval(pomodoroClock, 500);
-    
+    if(!edited){
+        initialMin--
+        min = initialMin;
+        sec = 59;
+        renderTitle("Pomodoro");
+        renderClock(initialMin, sec);
+        pomodoroInterval = setInterval(pomodoroClock, clockTick);
+        editBtn.style.display = "none";
+        saveBtn.style.display = "none";
+        resetBtn.style.display = "inline";
+    }else if(sec <= 0){
+        initialMin--
+        min = initialMin;
+        sec = 59;
+        renderTitle("Pomodoro");
+        pomodoroInterval = setInterval(pomodoroClock, clockTick);
+        editBtn.style.display = "none";
+        saveBtn.style.display = "none";
+        resetBtn.style.display = "inline";
+    }else{
+        min = initialMin;
+        renderTitle("Pomodoro");
+        pomodoroInterval = setInterval(pomodoroClock, clockTick);
+        editBtn.style.display = "none";
+        saveBtn.style.display = "none";
+        resetBtn.style.display = "inline";
+    }
 }
 function pomodoroClock(){
     if(sec <= 0 && min <= 0){
@@ -26,8 +90,8 @@ function pomodoroClock(){
         clockNotification("Hora da Pausa");
         renderTitle("Pausa");
         clearInterval(pomodoroInterval);
-        min = 1;
-        restInterval = setInterval(restClock, 500);
+        min = 5;
+        restInterval = setInterval(restClock, clockTick);
     }else if(sec <= 0){
         min--;
         sec = 59;
@@ -43,8 +107,8 @@ function restClock(){
         clockNotification("Hora do Pomodoro");
         renderTitle("Pomodoro");
         clearInterval(restInterval);
-        min = 1;
-        pomodoroInterval = setInterval(pomodoroClock, 500)
+        min = initialMin;
+        pomodoroInterval = setInterval(pomodoroClock, clockTick)
     }else if(sec <= 0){
         min--;
         sec = 59;
